e3252906a5b24d78f9432d9b4b86ea62a0079e69
NebyouT <nebyoutilikalem@gmail.com>
2025-02-28 01:49:56 +0300

    Your commit message

--- index.js
+++ index.js
@@ -21,12 +21,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// default middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173', 'https://e-learning-client.onrender.com'],
     credentials: true
 }));
 
